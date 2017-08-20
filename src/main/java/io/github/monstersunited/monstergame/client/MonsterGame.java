67fa68dc14d8e6329f54aabe5443c1ce385cc928
Ben O'Sullivan <s3662617@student.rmit.edu.au>
2017-08-20 14:32:59 +1000

    Created new maven project with support for intellij

---
+++ src/main/java/io/github/monstersunited/monstergame/client/MonsterGame.java
@@ -0,0 +1,7 @@
+package io.github.monstersunited.monstergame.client;
+
+public class MonsterGame {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
