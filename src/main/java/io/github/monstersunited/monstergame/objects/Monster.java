e8de988f7346c45b93913e7293c59e7e3c86c2a2
rmit-3642965-Tanzim-Shahriar <s3642965@student.rmit.edu.au>
2017-08-25 09:39:44 +1000

    Added Monster Tests

---
+++ src/main/java/io/github/monstersunited/monstergame/objects/Monster.java
@@ -0,0 +1,11 @@
+package io.github.monstersunited.monstergame.objects;
+
+public class Monster {
+    public int monsterCanEatPlayer(int i) {
+        return i+5;
+    }
+
+    public void monsterGoesToNearestPlayer() {
+
+    }
+}
