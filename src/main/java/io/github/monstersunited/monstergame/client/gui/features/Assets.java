467c421671e95c08cf6cd4b9681f856bde3cdac4
Simon Lay <lay00214@outlook.com>
2017-09-04 14:41:30 +1000

    Uses Tile Maps now and a brief start on collisions with the box object.

--- src/main/java/io/github/monstersunited/monstergame/client/gui/features/Assets.java
+++ src/main/java/io/github/monstersunited/monstergame/client/gui/features/Assets.java
@@ -1,5 +1,8 @@
 package io.github.monstersunited.monstergame.client.gui.features;
 
+
+import sun.awt.image.BufferedImageDevice;
+
 import java.awt.image.BufferedImage;
 
 public class Assets {
@@ -9,13 +12,14 @@ public class Assets {
     public static void init() {
         SpriteSheet sheet = new SpriteSheet(getResources.loadImage("/monster-tile08.png"));
 
-        tile= sheet.crop (0,0,288,288);
+        tile= sheet.crop (0,0,width,height);
         player1 =sheet.crop(288,0,width,height);
         player2 =sheet.crop(288+width,0,width,height);
         AI =sheet.crop(288+(width*2),0,width,height);
         player3 =sheet.crop(288,height,width,height);
-        player4 =sheet.crop(288+width,height*2,width,height);
-        box =sheet.crop(288+(width*2),height*3,width,height);
+        player4 =sheet.crop(288+width,height,width,height);
+        box =sheet.crop(288+(width*2),height,width,height);
 
     }
+
 }
---
+++ src/main/java/io/github/monstersunited/monstergame/client/gui/features/boardTile.java
@@ -0,0 +1,7 @@
+package io.github.monstersunited.monstergame.client.gui.features;
+
+public class boardTile extends TileGrid{
+    public boardTile(int id) {
+        super(Assets.tile,id);
+    }
+}
