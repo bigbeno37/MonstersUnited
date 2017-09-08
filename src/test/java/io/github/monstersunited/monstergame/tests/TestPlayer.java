af8d27eb51f01a21a2e75e8d2a76f73c0ab88187
boxhezi <s3654900@student.rmit.edu.au>
2017-09-08 11:19:55 +1000

    update testPlayer, add test where player is before processMove()

--- src/test/java/io/github/monstersunited/monstergame/tests/TestPlayer.java
+++ src/test/java/io/github/monstersunited/monstergame/tests/TestPlayer.java
@@ -18,13 +18,23 @@ public class TestPlayer {
 
         //initialize the position excepted
         int exceptX = player.getX();
-        int exceptY;
+        int exceptY = player.getY();
 
+        //check player position before testing
+        System.out.println("Checking the origin player position");
+        assertEquals(exceptX, player.getX());
+        assertEquals(exceptY, player.getY());
+
+        //initialize board
         Board board = new Board();
+
+        //update exceptY, check if there are some pieces on the position that player moving to
+        //if so, reject to move player
         if (player.getY() - 1 < 0) {
             //if the position that player going to move to has something on is already
             //keep the exceptY as where player standing at the moment
-            if(board.getPieceAt(player.getX(), 8) instanceof Entity) {
+            if (board.getPieceAt(player.getX(), 8) instanceof Entity) {
+                System.out.println("The position player move to has something on it already! Invalid Move");
                 exceptY = player.getY();
             } else {
                 exceptY = 8;
@@ -33,7 +43,7 @@ public class TestPlayer {
             exceptY = player.getY() - 1;
         }
 
-        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_W,'a',0);
+        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_W, 'a', 0);
 
         player.processMove(event, board);
         assertEquals(exceptX, player.getX());
@@ -44,7 +54,13 @@ public class TestPlayer {
     public void playerMoveDown() {
         Player player = new Player("Player", 0, 0, 1);
         int exceptX = player.getX();
-        int exceptY;
+        int exceptY = player.getY();
+
+        //check player position before testing
+        System.out.println("Checking the origin player position");
+        assertEquals(exceptX, player.getX());
+        assertEquals(exceptY, player.getY());
+
         Board board = new Board();
         if (player.getY() + 1 > 8) {
             if (board.getPieceAt(player.getX(), 0) instanceof Entity) {
@@ -55,7 +71,7 @@ public class TestPlayer {
         } else {
             exceptY = player.getY() + 1;
         }
-        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_S,'a',0);
+        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_S, 'a', 0);
 
         player.processMove(event, board);
         assertEquals(exceptX, player.getX());
@@ -65,8 +81,14 @@ public class TestPlayer {
     @Test
     public void playerMoveLeft() {
         Player player = new Player("Player", 0, 0, 1);
-        int exceptX;
+        int exceptX = player.getX();
         int exceptY = player.getY();
+
+        //check player position before testing
+        System.out.println("Checking the origin player position");
+        assertEquals(exceptX, player.getX());
+        assertEquals(exceptY, player.getY());
+
         Board board = new Board();
         if (player.getX() - 1 < 0) {
             if (board.getPieceAt(8, player.getY()) instanceof Entity) {
@@ -77,7 +99,7 @@ public class TestPlayer {
         } else {
             exceptX = player.getX() - 1;
         }
-        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_A,'a',0);
+        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_A, 'a', 0);
 
         player.processMove(event, board);
         assertEquals(exceptX, player.getX());
@@ -87,8 +109,14 @@ public class TestPlayer {
     @Test
     public void playerMoveRight() {
         Player player = new Player("Player", 0, 0, 1);
-        int exceptX;
+        int exceptX = player.getX();
         int exceptY = player.getY();
+
+        //check player position before testing
+        System.out.println("Checking the origin player position");
+        assertEquals(exceptX, player.getX());
+        assertEquals(exceptY, player.getY());
+
         Board board = new Board();
         if (player.getX() + 1 > 8) {
             if (board.getPieceAt(0, player.getY()) instanceof Entity) {
@@ -99,7 +127,7 @@ public class TestPlayer {
         } else {
             exceptX = player.getX() + 1;
         }
-        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_D,'a',0);
+        KeyEvent event = new KeyEvent(new Button(""), 0, 0, 0, KeyEvent.VK_D, 'a', 0);
 
         player.processMove(event, board);
         assertEquals(exceptX, player.getX());
