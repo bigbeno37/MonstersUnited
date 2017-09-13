e4c27a2ecfbfba28628d2a608239c75436be5f85
Simon Lay <s3658769@student.rmit.edu.au>
2017-09-13 18:27:28 +1000

    this adds the code for placing the walls in the correct order on the board

--- src/main/java/io/github/monstersunited/monstergame/objects/Wall.java
+++ src/main/java/io/github/monstersunited/monstergame/objects/Wall.java
@@ -4,11 +4,6 @@ import java.io.Serializable;
 
 public class Wall extends BoardPiece implements Serializable {
 
-    public Wall(int x, int y) {
-        collisionLocation();
-    }
-        //placeholder
-        //insert collisions
 
 
     public int collisionLocation(){
