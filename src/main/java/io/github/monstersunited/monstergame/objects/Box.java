8f08867226284736342aff7754c60c837f508441
Simon Lay <s3658769@student.rmit.edu.au>
2017-10-12 14:29:36 +1100

    Junit tests updated changed a bit

--- src/main/java/io/github/monstersunited/monstergame/objects/Box.java
+++ src/main/java/io/github/monstersunited/monstergame/objects/Box.java
@@ -4,13 +4,9 @@ import java.io.Serializable;
 
 public class Box extends BoardPiece implements Serializable {
     private int timer;
-    private int x;
-    private int y;
-    public Box(int time,int x, int y) {
+    public Box(int time) {
         this.timer = time;
-        this.x = x;
-        this.y =y;
-        super.setPosition(x,y);
+
     }
 
     public int getTimeLeft() {
