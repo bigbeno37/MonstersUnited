9810b767212a5267eb14dc5e1834cc0ab6a9869a
Simon Lay <s3658769@student.rmit.edu.au>
2017-10-12 18:26:54 +1100

    This should be cleaner version of the one i tried to commit before. I've gotten rid of the box functions

--- src/main/java/io/github/monstersunited/monstergame/client/gui/features/boardTile.java
+++
@@ -1,7 +0,0 @@
-package io.github.monstersunited.monstergame.client.gui.features;
-
-public class boardTile extends TileGrid{
-    public boardTile(int id) {
-        super(Assets.tile,id);
-    }
-}
--- src/test/java/io/github/monstersunited/monstergame/tests/TestDrawBoard.java
+++ src/test/java/io/github/monstersunited/monstergame/tests/TestDrawBoard.java
@@ -1,13 +1,9 @@
 package io.github.monstersunited.monstergame.tests;
 
-import io.github.monstersunited.monstergame.client.gui.features.TileGrid;
-import io.github.monstersunited.monstergame.client.gui.features.boxTile;
 import io.github.monstersunited.monstergame.client.gui.features.getResources;
 import org.junit.Test;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import static junit.framework.Assert.assertNotSame;
 import static junit.framework.TestCase.assertEquals;
